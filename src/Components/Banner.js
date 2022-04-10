419fefe38c387a45e3cb1a9f4ccfd6873f0eb1ee
Sannasichottu <sannasichottu@gmail.com>
2022-04-10 23:06:04 +0530

    depoly error sloved

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import Artist from "../img/artist.jpg";
 import Check from "../img/check.png";
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect } from "react";
 
 function Menu({ title, menuObject }) {
